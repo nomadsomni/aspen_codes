ab25a2d4a2ce801a53343f6bd19d01425e70de5f
nomadsomni <ayotusa@gmail.com>
2024-04-13 11:34:30 +0200

    aspen_codes:

--- daddy.ps1
+++ daddy.ps1
@@ -3,7 +3,16 @@ $daddy = Read-Host
 Write-Host "Podaj hasło"
 $haslo = Read-Host
 if ($haslo -eq $daddy) {
-    # Tutaj umieść kod, który ma być wykonany, gdy hasło jest poprawne
+    
+function Get-RandomIP {
+    return ("{0}.{1}.{2}.{3}" -f (Get-Random -Maximum 255), (Get-Random -Maximum 255), (Get-Random -Maximum 255), (Get-Random -Maximum 255))
+}
+
+# Wywołanie funkcji i wyświetlanie przykładowych adresów IP w nieskończonej pętli
+while ($true) {
+    Write-Output (Get-RandomIP)
+    Start-Sleep -Seconds 0.7
+}# Tutaj umieść kod, który ma być wykonany, gdy hasło jest poprawne
     Write-Host "Hasło poprawne! Skrypt zostanie wykonany."
 } else {
     Write-Host "Hasło niepoprawne! Skrypt nie zostanie wykonany."
